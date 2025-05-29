2425b3917a9e52b182874de7257e96a793be141b
Georgem Arruda <georgem.junior@meutudo.app>
2025-05-28 21:49:22 -0300

    Ajuste

--- backend/main.go
+++ backend/main.go
@@ -157,7 +157,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"POST", "GET", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type"},
 		ExposeHeaders:    []string{"Content-Length"},
